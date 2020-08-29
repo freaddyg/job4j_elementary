81bec41d9946e8f3c654d71aa86647cf472ceb1b
freaddyg <keepzor@mail.ru>
2020-08-29 15:25:36 +0300

    4.2. Тернарное сравнение [#345312]

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+import static org.hamcrest.core.Is.is;
+
+
+public class MaxTest {
+
+    @Test
+    public void maxTest() {
+        int result = Max.max(5, 17);
+        Assert.assertThat(result, is(17));
+    }
+}
