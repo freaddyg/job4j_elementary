af94b2c73b708481c3487710d41bd61adaab34f3
freaddyg <keepzor@mail.ru>
2020-08-31 13:52:12 +0300

    4.4. Вложенный оператор if else [#345313]

---
+++ src/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = 0;
+        if (first > second && first > third) {
+            result = first;
+
+
+        }else if (second > first && second > third) {
+
+            result = second;
+        }else {
+
+            result = third;
+        }
+
+        return result;
+    }
+}
