1aeb13eb589a7f813f4ff3b734fa18f18fcd53ef
freaddyg <keepzor@mail.ru>
2020-08-31 14:37:17 +0300

    4.6.1. Switch [#345309]

---
+++ test/ru/job4j/condition/SwitchWeekTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SwitchWeekTest {
+
+    @Test
+    public void nameOfDayTest() {
+
+
+        SwitchWeek days = new SwitchWeek();
+        String res = days.nameOfDay(5);
+        assertThat(res, is("Пятница"));
+
+    }
+}
