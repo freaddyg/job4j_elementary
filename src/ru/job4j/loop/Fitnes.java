b2c4bd423a9c286185a3ed560429bf626ce18488
freaddyg <keepzor@mail.ru>
2020-09-01 12:50:49 +0300

    5.4. Протеиновая диета [#345405] убрал лишнее

--- src/ru/job4j/loop/Fitnes.java
+++ src/ru/job4j/loop/Fitnes.java
@@ -3,11 +3,8 @@ package ru.job4j.loop;
 public class Fitnes {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        if (ivan <= nik) {
-            return 1;
-        }
 
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
             month++;
