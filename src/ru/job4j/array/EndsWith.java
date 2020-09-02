4a20cf52a90ba5b374a79d0e7c44ba9af1fa4d76
freaddyg <keepzor@mail.ru>
2020-09-02 17:10:22 +0300

    6.4.2. Слово заканчивается на ... [#345324]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - 2 + i] != post[i]) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ test/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+
+    @Test
+    public void endsWithTestTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void endsWithTestFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
