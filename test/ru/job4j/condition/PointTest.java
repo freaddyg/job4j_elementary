3554810d77dae965ee14fbf2b933a2bf6b95e6ab
freaddyg <keepzor@mail.ru>
2020-09-07 15:26:06 +0300

    4. Расстояние между точками в трехмерном пространстве. [#345490]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -3,49 +3,23 @@ package ru.job4j.condition;
 import org.junit.Test;
 import org.junit.Assert;
 
-//public class PointTest {
-//
-//    @Test
-//    public void when00to20then2() {
-//        int expected = 2;
-//        int x1 = 0;
-//        int y1 = 0;
-//        int x2 = 2;
-//        int y2 = 0;
-//        double out = Point.distance(x1, y1, x2, y2);
-//        Assert.assertEquals(expected, out, 0.01);
-//    }
-//
-//    @Test
-//    public void when72to109then76() {
-//        double expected = 7.6;
-//        int x1 = 7;
-//        int y1 = 2;
-//        int x2 = 10;
-//        int y2 = 9;
-//        double out = Point.distance(x1, y1, x2, y2);
-//        Assert.assertEquals(expected, out, 0.1);
-//    }
-//
-//    @Test
-//    public void when03to41then447() {
-//        double expected = 4.47;
-//        int x1 = 0;
-//        int y1 = 3;
-//        int x2 = 4;
-//        int y2 = 1;
-//        double out = Point.distance(x1, y1, x2, y2);
-//        Assert.assertEquals(expected, out, 0.01);
-//    }
-//
-//    @Test
-//    public void when58to14then56() {
-//        double expected = 5.6;
-//        int x1 = 5;
-//        int y1 = 8;
-//        int x2 = 1;
-//        int y2 = 4;
-//        double out = Point.distance(x1, y1, x2, y2);
-//        Assert.assertEquals(expected, out, 0.1);
-//    }
-//}
+public class PointTest {
+
+    @Test
+    public void distanseTest2D() {
+        int expected = 2;
+        Point first = new Point(2, 0);
+        Point second = new Point(0, 0);
+        double out = first.distance(second);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distanseTest3D() {
+        double expected = 2.8;
+        Point first = new Point(2, 0, 6);
+        Point second = new Point(0, 2, 4);
+        double out = first.distance(second);
+        Assert.assertEquals(expected, out, 0.1);
+    }
+}
