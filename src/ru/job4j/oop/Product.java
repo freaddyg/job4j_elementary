f097adc8eac36c4af6f43ea50d3b33dc9a909042
freaddyg <keepzor@mail.ru>
2020-09-09 18:40:07 +0300

    6. Почему наследование - это плохо [#345525] Product

---
+++ src/ru/job4j/oop/Product.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Product {
+    String name;
+    int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
